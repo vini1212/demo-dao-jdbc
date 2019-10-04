874d4325520307fd6cea7d71a0d74858dadcaed9
Vinicius Geraldo <vinigeraldo36@gmail.com>
2019-10-04 15:45:13 -0300

    findAll implementação

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -104,8 +104,49 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null; //aponta para posição 0
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "order by Name");
+			
+			/*Esse comando sql vai ser executado e o resultado vai cair dentro do meu ResultSet
+			 * */
+
+			rs = st.executeQuery(); //executa uma consulta sql
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>(); //criou um map vazio para guardar qualquer departamento que instanciar
+			
+			while(rs.next()) { //tem que percorrer meu ResultSet enquanto tiver um próximo
+				//se não existir ai sim vou precisar instanciar o departamento
+				Department dep = map.get(rs.getInt("DepartmentId")); //tenta buscar com o método get um departamento que tenha esse Id
+				
+				//se o departamento não existir ai sim ele vai instanciar e vai salvar o departamento no map 
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+								 
+				Seller obj = instantiateSeller(rs, dep);	
+				list.add(obj);
+				
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+			//não precisa fechar a conexão, porque o mesmo objeto pode servir para realizar mais de uma operação 
+		}
 	}
 
 	@Override
